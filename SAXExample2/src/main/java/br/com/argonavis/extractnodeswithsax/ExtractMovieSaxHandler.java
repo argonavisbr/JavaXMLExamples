d0501571fdb84e00fb580b7a646574496ab19036
helderdarocha <helder.darocha@gmail.com>
2014-02-13 14:12:53 -0200

    Fixed a bug

--- SAXExample2/src/main/java/br/com/argonavis/extractnodeswithsax/ExtractMovieSaxHandler.java
+++ SAXExample2/src/main/java/br/com/argonavis/extractnodeswithsax/ExtractMovieSaxHandler.java
@@ -85,7 +85,7 @@ class ExtractMovieSaxHandler extends DefaultHandler {
         // Rebuild the XML if it's a node we didn't skip
         if (qName.equals("Movie")) {
             if (!skip) {
-                result.add(contents.toString());
+                result.add(contents.insert(0,"<Movie>").append("</Movie>").toString());
             }
 
             // reset the variables so we can check the next node
